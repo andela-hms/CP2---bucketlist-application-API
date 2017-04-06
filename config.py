f356cdd8f2c5d4eb40d796886cfd56d2180cf7bc
andela-hms <humphrey.musonye@andela.com>
2017-04-06 03:33:06 +0300

    Setup app configuration

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'new-key-here'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///bucketlist.db"
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
