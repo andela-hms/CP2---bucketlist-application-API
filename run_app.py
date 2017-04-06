a737b4cb8ce28cce9b40eba60492fdd31de5b537
andela-hms <humphrey.musonye@andela.com>
2017-04-06 14:09:33 +0300

    Merge with db_setup

--- run_app.py
+++ run_app.py
@@ -1,2 +1,17 @@
-import db_setup
-from models import User, BucketList, Item
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the app
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from app.bucket_list import blueprint
+# register blueprint as part of the app
+app.register_blueprint(blueprint)
+
+if __name__ == '__main__':
+    app.run()
