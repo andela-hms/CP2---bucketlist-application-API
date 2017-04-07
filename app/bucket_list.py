264af53ec5b6a7d3cde710a7b524cb0ee7376550
andela-hms <humphrey.musonye@andela.com>
2017-04-07 11:11:48 +0300

    Enable authentication

--- app/bucket_list.py
+++ app/bucket_list.py
@@ -1,12 +1,24 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, g, request
 from flask_restful import Api, Resource, reqparse, fields, marshal
-
-from models import BucketList, Item
+from flask_httpauth import HTTPTokenAuth
+from models import BucketList, Item, User
 from run_app import app, db
 
 # Declare Blueprint
 blueprint = Blueprint('bucket_list', __name__)
 api = Api(blueprint)
+
+auth_user = HTTPTokenAuth(scheme="Token")
+
+@auth_user.verify_token
+def verify_token(token):
+
+    # verify user token
+    user = User.verify_auth_token(token)
+    if not user:
+        return False
+    g.user = user
+    return True
 
 item_fields = { 'item_id' : fields.Integer,
                         'item_name': fields.String,
@@ -27,36 +39,40 @@ bucket_list_fields = { 'bucketlist_id' : fields.Integer,
 
 class BucketListAPI(Resource):
     """ Creates endpoints for BucketListAPI """
+
+    decorators = [auth_user.login_required]
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('bucketlist_name', type = str, required = True,
-            help = 'No BucketList name provided', location = 'json')
+        self.reqparse.add_argument('bucketlist_name', type=str, required=True, help='No BucketList name provided', location = 'json')
         super(BucketListAPI, self).__init__()
-    
+
     def get(self, id=None):
 
         """ List all the created bucket lists and single ones too """
         if id:
-            this_bucket_list = BucketList.query.filter_by(bucketlist_id=id).first()
-            return this_bucket_list
+            this_bucket_list = BucketList.query.filter_by(bucketlist_id=id, created_by=g.user.user_id).first()
+            return marshal(this_bucket_list, bucket_list_fields), 200        
         else:
-            self.reqparse.add_argument('q', type = str, location = 'json')
-            self.reqparse.add_argument('limit', type = int, location = 'json', default=20)
-            self.reqparse.add_argument('page', type = int, location = 'json', default=1)
+            
+            self.reqparse = reqparse.RequestParser()
+            self.reqparse.add_argument('q', type = str, location = 'args')
+            self.reqparse.add_argument('limit', type = int, location = 'args',default=20)
+            self.reqparse.add_argument('page', type = int, location = 'args', default=1)
 
             args = self.reqparse.parse_args()
             q = args['q']
             limit = args['limit']
             page = args['page']
-
+            
             if q:
-                bucketlists = BucketList.query.filter_by(BucketList.bucketlist_name.like('%'+q+'%')).paginate(page, limit, False)
+                bucketlists = BucketList.query.filter(BucketList.bucketlist_name.like('%'+q+'%'), BucketList.created_by==g.user.user_id).paginate(page, limit, False)
             else:
-                bucketlists = BucketList.query.all().paginate(page, limit, False)
+                bucketlists = BucketList.query.filter_by(created_by=g.user.user_id).paginate(page, limit, False)
             
             if not bucketlists:
                 return {'message': 'bucketlists not found'}, 404
-            
+
             if bucketlists.has_prev:
                 prev_page = request.url + '?page=' + str(page + 1) + '&limit=' + str(limit)
             else:
@@ -69,8 +85,8 @@ class BucketListAPI(Resource):
 
             return { 'message': {'next_page':next_page,
                                 'prev_page': prev_page,
-                                'total': bucketlists.pages,
-                                'bucketlists': marshal(bucketlists.items, bucket_list_fields)}}, 200
+                                'total': bucketlists.pages},
+                                'bucketlists': marshal(bucketlists.items, bucket_list_fields)}, 200
 
 
     def post(self):
@@ -82,8 +98,8 @@ class BucketListAPI(Resource):
 
         if check_duplicate:
             return {'error': 'bucketlist_name {} already exists'.format(name)}
-        
-        new_bucketlist = BucketList(bucketlist_name=name)
+
+        new_bucketlist = BucketList(bucketlist_name=name, created_by=g.user.user_id)
         db.session.add(new_bucketlist)
         db.session.commit()
 
@@ -116,6 +132,8 @@ class BucketListAPI(Resource):
 
 class ItemAPI(Resource):
     """ Creates endpoints for ItemAPI """
+    decorators = [auth_user.login_required]    
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('item_name', type = str, required = True,
@@ -167,5 +185,6 @@ class ItemAPI(Resource):
 
         return {'message': 'Item with id {} has been deleted'.format(item_id)}
 
-api.add_resource(BucketListAPI, '/bucketlists/', '/bucketlists/<int:id>/', endpoint='bucketlists')
+api.add_resource(BucketListAPI, '/bucketlists/<int:id>/', endpoint='bucketlist')
+api.add_resource(BucketListAPI, '/bucketlists/', endpoint='bucketlists')
 api.add_resource(ItemAPI, '/bucketlists/<int:bucketlist_id>/items/', '/bucketlists/<int:bucketlist_id>/items/<int:item_id>', endpoint='items')
