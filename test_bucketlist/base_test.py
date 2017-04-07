fab6aaca63399f501cdb3ca7ca938d2937ea5ff9
andela-hms <humphrey.musonye@andela.com>
2017-04-07 16:36:15 +0300

    Create test structure

---
+++ test_bucketlist/base_test.py
@@ -0,0 +1,29 @@
+import os
+from flask_testing import TestCase
+
+
+from run_app import app, db
+from models import User
+
+class BaseTestCase(TestCase):
+
+    def create_app(self):
+        app.config.from_object('config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        new_user = User(
+            username='testuser',
+            email_address="test@user.com",
+        )
+        new_user.hash_this_pass("thispass")
+        db.session.add(new_user)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        os.remove('test.db')
+
+
---
+++ test_bucketlist/test_bucket_list.py
@@ -0,0 +1,27 @@
+import json
+from base_test import BaseTestCase
+
+URL = "/auth/"
+class AuthTestCase(BaseTestCase):
+    def test_create_bucket_list(self):
+        """ Tests whether a user can login """
+        data = json.dumps(dict(
+                email="test@user.com",
+                password="thispass"
+        ))
+
+        headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
+        response = self.client.post(URL+"login/", data=data, content_type="application/json")
+        self.assertIn("auth_token",response.data.decode())
+
+    def test_register(self):
+        """ Tests whether a new user can be created successfully """
+        data = json.dumps(dict(
+            username="new_user",
+            email="new_user@user.com",
+            password="newuserpass"
+            ))
+
+        headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
+        response = self.client.post(URL+"register/", data=data, content_type="application/json")
+        self.assertIn("Registration successful",response.data.decode())
