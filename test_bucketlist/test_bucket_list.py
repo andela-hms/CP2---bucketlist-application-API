4fc473d4b4b2c342ef3a13c524fe4c4ec120d296
andela-hms <humphrey.musonye@andela.com>
2017-04-09 19:06:24 +0300

    Test for bucketlist creation

--- test_bucketlist/test_bucket_list.py
+++ test_bucketlist/test_bucket_list.py
@@ -1,27 +1,16 @@
 import json
 from base_test import BaseTestCase
 
-URL = "/auth/"
-class AuthTestCase(BaseTestCase):
+URL = "/bucketlists/"
+class BucketListestCase(BaseTestCase):
     def test_create_bucket_list(self):
-        """ Tests whether a user can login """
+        """ Tests whether a user can create a bucketlist """
         data = json.dumps(dict(
-                email="test@user.com",
-                password="thispass"
+                bucketlist_name="Kenya"
         ))
 
         headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
-        response = self.client.post(URL+"login/", data=data, content_type="application/json")
-        self.assertIn("auth_token",response.data.decode())
+        response = self.client.post(URL, data=data, content_type="application/json")
+        self.assertIn(" ",response.data.decode())
 
-    def test_register(self):
-        """ Tests whether a new user can be created successfully """
-        data = json.dumps(dict(
-            username="new_user",
-            email="new_user@user.com",
-            password="newuserpass"
-            ))
-
-        headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
-        response = self.client.post(URL+"register/", data=data, content_type="application/json")
-        self.assertIn("Registration successful",response.data.decode())
+        def 
