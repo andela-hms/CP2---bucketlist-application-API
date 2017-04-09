be15277f7df097ce70f3121e026231d5608680de
andela-hms <humphrey.musonye@andela.com>
2017-04-10 02:57:23 +0300

    Remove unused variables

--- test_bucketlist/test_bucket_list.py
+++ test_bucketlist/test_bucket_list.py
@@ -26,9 +26,7 @@ class BucketListTestCase(BaseTestCase):
             password="thispass"
         ))
 
-        headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
         response = self.client.post("/auth/login/", data=data, content_type="application/json")
-        # response = response.data.decode()
         data = json.loads(response.data.decode())
         auth_token = data['auth_token']
 
@@ -44,8 +42,8 @@ class BucketListTestCase(BaseTestCase):
             bucketlist_name="Kenya"
         ))
 
-        headers = json.dumps({"Content-Type":"application/json","Accept":"application/json"})
-        response = self.client.post(URL, data=new_bucket_list, content_type="application/json", headers=self.headers)
+        response = self.client.post(URL, data=new_bucket_list, \
+        content_type="application/json", headers=self.headers)
         self.assertIn("bucketlist_name", response.data.decode())
 
     def tearDown(self):
@@ -53,3 +51,4 @@ class BucketListTestCase(BaseTestCase):
         db.session.remove()
         db.drop_all()
         os.remove('test.db')
+        
