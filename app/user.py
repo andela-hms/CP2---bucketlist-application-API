0cb58a3f6af2567ce9898ceff45b30522e3432c4
andela-hms <humphrey.musonye@andela.com>
2017-04-10 05:41:03 +0300

    Fix pep8 errors

--- app/user.py
+++ app/user.py
@@ -8,19 +8,21 @@ from run_app import app, db
 user_blueprint = Blueprint('user_endpoint', __name__)
 auth_api = Api(user_blueprint)
 
-user_fields = { 'user_id': fields.Integer,
-                        'username': fields.String,
-                        'email_address': fields.String,
-                        'password_hash': fields.String
+user_fields = {
+    'user_id': fields.Integer,
+    'username': fields.String,
+    'email_address': fields.String,
+    'password_hash': fields.String
 }
 
 class LoginAPI(Resource):
     """ Creates endpoints for LoginAPI """
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('email', type = str, required=True, help = 'No email address provided', location = 'json')
-        self.reqparse.add_argument('password', type = str, required = True,
-            help = 'No password provided', location = 'json')            
+        self.reqparse.add_argument('email', type=str, required=True, \
+        help='No email address provided', location='json')
+        self.reqparse.add_argument('password', type=str, required=True, \
+        help='No password provided', location='json')
 
     def post(self):
         """ Logs a user in """
@@ -39,7 +41,7 @@ class LoginAPI(Resource):
                         'auth_token': give_token.decode()
                     }
                     return json_response, 200
-                
+
                 else:
                     json_response = {
                         'error': 'Invalid Password'
@@ -47,24 +49,26 @@ class LoginAPI(Resource):
                     return json_response, 401
             else:
                 json_response = {
-                        'error': 'User with email {} does not exist'.format(args['email'])
-                    }
+                    'error': 'User with email {} does not exist'.format(args['email'])
+                }
                 return json_response, 404
         except:
             json_response = {
-                        'error': 'Login Failed'
-                    }
+                'error': 'Login Failed'
+            }
             return json_response, 500
 
 
 class RegisterAPI(Resource):
-
     """ Creates endpoints for RegisterAPI """
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('username', type = str, required = True, help = 'No username provided', location = 'json')           
-        self.reqparse.add_argument('email', type=inputs.regex(r"[^@]+@[^@]+\.[^@]+"), required=True, help="Invalid email format", location='json')
-        self.reqparse.add_argument('password', type = str, required = True, help = 'No password provided', location = 'json')
+        self.reqparse.add_argument('username', type=str, required=True, \
+        help='No username provided', location='json')
+        self.reqparse.add_argument('email', type=inputs.regex(r"[^@]+@[^@]+\.[^@]+"), \
+        required=True, help="Invalid email format", location='json')
+        self.reqparse.add_argument('password', type=str, required=True, \
+        help='No password provided', location='json')
         super(RegisterAPI, self).__init__()
 
     def post(self):
@@ -77,7 +81,8 @@ class RegisterAPI(Resource):
 
         if not check_user:
             try:
-                check_user = User(username=args['username'], email_address=args['email'], password_hash=args['password'])
+                check_user = User(username=args['username'], \
+                email_address=args['email'], password_hash=args['password'])
                 # hash password before saving to db
                 check_user.hash_this_pass(args['password'])
 
@@ -91,9 +96,8 @@ class RegisterAPI(Resource):
                 # message on successful registration
                 json_response = {
                     'message' : 'Registration successful',
-                    'token' : 'token.decode()'
                 }
-                
+
                 return json_response, 201
 
             except:
@@ -105,8 +109,8 @@ class RegisterAPI(Resource):
         else:
             # user exists
             json_response = {
-                    'message' : 'User already exists'
-                }
+                'message' : 'User already exists'
+            }
 
             return json_response, 409
 
