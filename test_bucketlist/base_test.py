2691fc25aa55e57888286961bcca7c900626b487
andela-hms <humphrey.musonye@andela.com>
2017-04-10 11:15:49 +0300

    Add token auth

--- test_bucketlist/base_test.py
+++ test_bucketlist/base_test.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask_testing import TestCase
 
 
@@ -21,6 +22,22 @@ class BaseTestCase(TestCase):
         new_user.hash_this_pass("thispass")
         db.session.add(new_user)
         db.session.commit()
+
+        data = json.dumps(dict(
+            email="test@user.com",
+            password="thispass"
+        ))
+
+        response = self.client.post("/api/v1.0/auth/login/", \
+        data=data, content_type="application/json")
+        data = json.loads(response.data.decode())
+        auth_token = data['auth_token']
+
+        self.headers = {
+            'Authorization':'Token ' + auth_token,
+            'Content-Type': 'application/json',
+            'Accept':'application/json',
+        }
 
     def tearDown(self):
         """ Clear resources after tests are run """
