4140ff7f1a714fa850a44d0c5699cb2b0b19862b
andela-hms <humphrey.musonye@andela.com>
2017-04-10 08:05:06 +0300

    Fix pep8 errors

--- run_app.py
+++ run_app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the app
+# initialize the app
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,8 +11,11 @@ app.url_map.strict_slashes = False
 db = SQLAlchemy(app)
 
 from app.bucket_list import blueprint
-# register blueprint as part of the app
 app.register_blueprint(blueprint)
+
+from app.user import user_blueprint
+app.register_blueprint(user_blueprint)
 
 if __name__ == '__main__':
     app.run()
+    
